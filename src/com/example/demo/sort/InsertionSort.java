9064708bbac6ee75632966087a799590dce73c0c
Original <2718771714@qq.com>
2020-06-05 17:20:03 +0800

    algorithm

---
+++ src/com/example/demo/sort/InsertionSort.java
@@ -0,0 +1,35 @@
+package com.example.demo.sort;
+
+//插入排序
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] a=new int [] {5,8,2,4,9,3,1,0,6,7};
+        printAns(a);
+        insertionSort(a);
+        printAns(a);
+    }
+    //取出当前元素与之前元素比较 如果前面元素大于目前，前面元素就后移 直到插入成功
+    //warings : for(int j=i-1;j>=0;j--)  需要index t来记录下标以及a[i] index不可以省略 因为if else里无法赋值a[j]=t 因为a[j]始终比t大
+    public static void insertionSort(int [] a){
+        for(int i=0;i<a.length;i++){
+            int t=a[i];
+            int index=i;
+            for(int j=i-1;j>=0;j--){
+                if(a[j]>t){
+                    a[j+1]=a[j];
+                    index=j;
+                }
+            }
+            a[index]=t;
+        }
+    }
+
+    public static void printAns(int [] a){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/com/example/demo/sort/SelectionSort.java
@@ -0,0 +1,33 @@
+package com.example.demo.sort;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] a=new int [] {5,8,2,4,9,3,1,0,6,7};
+        printAns(a);
+        selectionSort(a);
+        printAns(a);
+    }
+    //使用index下标进行记录 求出最小的值 并和a[i]交换
+    // warings: for(int j=i;j<a.length;j++)
+    public static void selectionSort(int [] a){
+        for(int i=0;i<a.length;i++){
+            int index=i;
+            for(int j=i;j<a.length;j++){
+                if (a[index]>a[j]){
+                    index=j;
+                }
+            }
+            int t=a[i];
+            a[i]=a[index];
+            a[index]=t;
+        }
+    }
+
+    public static void printAns(int [] a){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+}
