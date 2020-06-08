8fa9f696b3adec286bf1e5a2991d8f44538ea8ca
Original <2718771714@qq.com>
2020-06-08 14:09:45 +0800

    offer-heap

--- src/com/example/demo/leetcode/offer/heap/Title41.java
+++ src/com/example/demo/leetcode/offer/heap/Title41.java
@@ -1,7 +1,10 @@
 package com.example.demo.leetcode.offer.heap;
 
 
-/*
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*  面试题41. 数据流中的中位数
 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值
 如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
 
@@ -29,4 +32,54 @@ double findMedian() - 返回目前所有元素的中位数
 
  */
 public class Title41 {
+    public static void main(String[] args) {
+        MedianFinder medianFinder=new MedianFinder();
+        medianFinder.addNum(1);
+        medianFinder.addNum(2);
+        System.out.println(medianFinder.findMedian());
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian());
+    }
+}
+
+/*
+用大顶堆+小顶堆方法，可以看作大顶堆是普通班，小顶堆是实验班。数量上时刻保持 小顶-大顶<=1（两堆相等或者小顶比大顶多一个）。
+新学生先入普通班（大顶堆），此时可能会失去平衡了，于是取大顶堆的第一个（班里最好的学生）加入实验班（小顶堆），
+判断若数量过多（不是等于或多一个），取第一个（实验班里最差的学生）到普通班（大顶堆）里。 取中位数的时候，若两堆数量相等，则各取堆顶取平均，
+若小顶比大顶多一，则多的那一个就是中位数。
+ */
+class MedianFinder {
+    //PriortityQueue 内部实现 堆排序
+    private PriorityQueue<Integer> maxHeap;
+    private PriorityQueue<Integer> minHeap;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        //大堆
+        maxHeap=new PriorityQueue<>(11, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;//o2-o1 前面的值 - 后面的值 ： 0 相等 | -1 o1 o2倒序排序 | 1 o1 o2正序排序
+            }
+        });
+        //小堆
+        minHeap=new PriorityQueue<>();
+
+    }
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(maxHeap.size()+1<minHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if(maxHeap.size() < minHeap.size()){
+            return (double) minHeap.peek();
+        }else{
+            return (double) (maxHeap.peek()+minHeap.peek())/2;
+        }
+    }
 }
