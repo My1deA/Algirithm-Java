4c4508834c090c37a369951e0bb7d0b4b4fb7a41
Original <2718771714@qq.com>
2020-06-11 23:02:28 +0800

    offer-hash

--- src/com/example/demo/leetcode/offer/Mark.java
+++ src/com/example/demo/leetcode/offer/Mark.java
@@ -2,7 +2,7 @@ package com.example.demo.leetcode.offer;
 
 public class Mark {
     public static void main(String[] args) {
-        String[] str={"16","26","37","28","41","45","59-1","59-2","68-1","68-2"};
-        String[] str2={"54"};
+        String[] str={"16","26","37","28","41","45","54","59-1","59-2","68-1","68-2"};
+        String[] dp={"48"};
     }
 }
