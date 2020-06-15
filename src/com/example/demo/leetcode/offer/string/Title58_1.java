251564652201982bb455e716d6422310178c87ed
Original <2718771714@qq.com>
2020-06-15 10:09:32 +0800

    offer-string

---
+++ src/com/example/demo/leetcode/offer/string/Title58_1.java
@@ -0,0 +1,30 @@
+package com.example.demo.leetcode.offer.string;
+
+
+import sun.security.util.Length;
+
+/* 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，
+标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+ */
+public class Title58_1 {
+    public static void main(String[] args) {
+        String s="I am a   student. ";
+        Solution58_1 solution58_1=new Solution58_1();
+        System.out.println(solution58_1.reverseWords(s));
+    }
+}
+
+class Solution58_1 {
+    public String reverseWords(String s) {
+        String[] str=s.split(" ");
+        StringBuffer stringBuffer=new StringBuffer();
+        for(int i=str.length-1;i>=0;i--){
+            if(str[i].equals("")){
+                continue;
+            }
+            stringBuffer.append(str[i]+" ");
+
+        }
+        return stringBuffer.toString().trim();
+    }
+}
---
+++ src/com/example/demo/leetcode/offer/string/Title58_2.java
@@ -0,0 +1,26 @@
+package com.example.demo.leetcode.offer.string;
+
+/*  面试题58 - II. 左旋转字符串
+字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ */
+public class Title58_2 {
+    public static void main(String[] args) {
+        String s="abcdefg";
+        Solution58_2 solution58_2=new Solution58_2();
+        System.out.println(solution58_2.reverseLeftWords(s,2));
+    }
+}
+
+class Solution58_2 {
+    public String reverseLeftWords(String s, int n) {
+        StringBuffer stringBuffer=new StringBuffer();
+        for(int i=n;i<s.length();i++){
+            stringBuffer.append(s.charAt(i));
+        }
+        for(int i=0;i<n;i++){
+            stringBuffer.append(s.charAt(i));
+        }
+        return stringBuffer.toString();
+    }
+}
