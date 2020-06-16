1438f7fb7b7eb97b31df23ffef11084ce8d240f1
Original <2718771714@qq.com>
2020-06-16 16:15:54 +0800

    offer-dp

--- src/com/example/demo/leetcode/offer/Mark.java
+++ src/com/example/demo/leetcode/offer/Mark.java
@@ -4,6 +4,6 @@ public class Mark {
     public static void main(String[] args) {
         String[] str={"16","26","37","28","41","45","54","59-1","59-2","68-1","68-2"};
         String[] str1={"48","35",};
-        String[] undone={"20","67"};
+        String[] undone={"20","67","19"};
     }
 }
