5995441599f19c84d8fedf456a47403d1a791004
Original <2718771714@qq.com>
2020-06-22 22:48:44 +0800

    offer-more

---
+++ src/com/example/demo/leetcode/offer/more/Title57_2.java
@@ -0,0 +1,17 @@
+package com.example.demo.leetcode.offer.more;
+
+/* 剑指 Offer 57 - II. 和为s的连续正数序列
+输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+输入：target = 9
+输出：[[2,3,4],[4,5]]
+ */
+class Solution {
+    public int[][] findContinuousSequence(int target) {
+        return null;
+    }
+}
+
+public class Title57_2 {
+}
