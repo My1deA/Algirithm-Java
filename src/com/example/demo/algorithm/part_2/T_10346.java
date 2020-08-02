3c595970252003149750ac012bbee1e26c24e5a6
Original <2718771714@qq.com>
2020-08-02 14:37:39 +0800

    Algorithm 11-20

--- src/com/example/demo/algorithm/part_2/T_10346.java
+++ src/com/example/demo/algorithm/part_2/T_10346.java
@@ -1,7 +1,7 @@
 package com.example.demo.algorithm.part_2;
 
 
-// 10346 带价值作业安排问题  贪心
+// 10346 带价值作业安排问题  贪心 hash
 /*Description
         已知n项作业E={1, 2, … ,n} 需要完成，只有一台机器，同一时刻至多完成一个作业，而且每项作业需要的时间都是单位时间1。第k项作业要求在时间fk时刻执行并完成，而且
         完成这项作业将获得效益pk，（k=1, 2, … , n）。E的子集称为相容的，如果它们可以被安排由一台机器完成。
@@ -17,14 +17,14 @@ package com.example.demo.algorithm.part_2;
 
         例如：7个作业
         时间点和效益值分别是：
-        1  8 8  5 9  3  5
+        1  8   8  5 9  3  5
         20 25 30 7 18 10 18
         则：可以获得的最大总效益为：20 + 30 +18 + 10 + 18 = 96
 
         输入样例                        输出样例
         7                              96
-        1 88 5 9 3 5
-        2025 30 7 18 10 18
+        1 8 8 5 9 3 5
+        20 25 30 7 18 10 18
 提示
         贪心法
         1）按价值从高到低排序所有作业；
@@ -33,5 +33,69 @@ package com.example.demo.algorithm.part_2;
         此处，相容的判断不同于书上的活动安排问题，因为每个作业仅需一个单位时间就可完成，所以每当判断一个新的作业是否可以加入进已有的相容作业集合，就看这个新的作业是否
         能在其执行时间内被安排。*/
 
+// 其实可以用hash来做
 public class T_10346 {
+
+    public static int hashset(int[] e,int[] fk){
+        int[] exist=new int[e.length];
+        sort(e,fk);
+        int sum=0;
+        for(int i=0;i<e.length;i++){
+            if(!isExist(exist,e[i])){
+                //System.out.print(fk[i]+" ");
+                sum+=fk[i];
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isExist(int[] exist,int n){
+
+        for(int i=0;i< exist.length;i++){
+            if(exist[i]==n){
+                return true;
+            }else if(exist[i]==0) {
+                exist[i]=n;
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+    //先拍下
+    public static void sort(int[] e , int[] fk){
+        for(int i=0;i<fk.length;i++){
+            for(int j=0;j<fk.length-1;j++){
+                if(fk[j]<fk[j+1]){
+                    swap(e,fk,j,j+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] e,int[] fk, int i,int j){
+        int t=e[i];
+        e[i]=e[j];
+        e[j]=t;
+
+        t=fk[i];
+        fk[i]=fk[j];
+        fk[j]=t;
+    }
+
+
+    public static void main(String[] args) {
+        int[] e={1,8,8,5,9,3,5};
+        int[] fk={20,25,30,7,18,10,18};
+        System.out.println(hashset(e,fk));
+    }
 }
+
+
+
+
+
+
+
+
