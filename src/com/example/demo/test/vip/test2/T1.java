055cc0aa20d7345e203f688f5548fd3b875f35e5
Original <2718771714@qq.com>
2020-08-11 22:30:51 +0800

    Algorithm - Test - vip

--- src/com/example/demo/test/vip/test2/T1.java
+++ src/com/example/demo/test/vip/test2/T1.java
@@ -1,56 +1,44 @@
 package com.example.demo.test.vip.test2;
 
+/*输入一个字符串，输出该字符串中相邻字符的所有组合。
+        举个例子，如果输入abc，它的组合有a、b、c、ab、bc、abc。（注意：输出的组合需要去重）（40分）
 
-/*输入两个字符串a和b，字符串内容为二进制数字，求两个字符串相加的结果，加法计算方法以二进制方式计算，并返回对应的字符串结果。要求程序尽可能的高效。示例如下：
+        输入描述:
+        一个字符串
 
-输入描述:
-        输入两个字符串，如"1101", "1100"
+        输出描述:
+        一行，每个组合以空格分隔，相同长度的组合需要以字典序排序，且去重。
 
-输出描述:
-        "11001"
+        输入例子1:
+        bac
 
-输入例子1:
-        1101 1100
-
-输出例子1:
-        11001
-*/
+        输出例子1:
+        a b c ac ba bac*/
 
 
-import java.util.Scanner;
+import java.util.*;
 
 public class T1 {
 
-    public static String add(String a, String b){
+    public static void getAns(String str){
         StringBuffer stringBuffer=new StringBuffer();
-        Integer a1=Integer.parseInt(a);
-        Integer b1=Integer.parseInt(b);
-        int carry=0;
-        while(a1!=0 || b1!=0 || carry!=0 ){
-            int e1=a1%10;
-            a1=a1/10;
-
-            int e2=b1%10;
-            b1=b1/10;
-
-            int sum=e1+e2+carry;
-            if(sum>=2){
-                carry=1;
-                sum=sum-2;
-            }else{
-                carry=0;
+        for(int i=1;i<=str.length();i++){
+            TreeSet<String> sortSets=new TreeSet<>();
+            for(int j=0;j+i<=str.length();j++){
+                sortSets.add(str.substring(j,j+i));
+            }
+            for(String s:sortSets){
+                stringBuffer.append(s).append(" ");
             }
 
-            stringBuffer.append(sum);
         }
-
-        return stringBuffer.reverse().toString();
+        System.out.println(stringBuffer);
     }
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        String a=scanner.next();
-        String b=scanner.next();
-        System.out.println(add(a,b));
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        getAns(str);
+
     }
 }
--- src/com/example/demo/test/vip/test2/T2.java
+++ src/com/example/demo/test/vip/test2/T2.java
@@ -1,70 +1,69 @@
 package com.example.demo.test.vip.test2;
 
 
-/*给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
-请注意，它是排序后的第k小元素，而不是第k个元素。
-
-示例:
-        matrix = [
-        [ 1,  5,  9],
-        [10, 11, 13],
-        [12, 13, 15]
-        ],
-        k = 8,
-
-        返回 13。
-        说明:
-        你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2 。
+/*数独是一个非常有名的游戏。整个是一个9X9的大宫格，其中又被划分成9个3X3的小宫格。要求在每个小格中放入1-9中的某个数字。要求是：每行、每列、每个小宫格中数字不能重复。 现要求用计算机求解数独。（50分）
 
         输入描述:
-        第一行为k的值和矩阵的n的值 后续为n*n矩阵的数字，以空格分割
-
-
+        输入9行，每行为空格隔开的9个数字，为0的地方就是需要填充的数字。
 
         输出描述:
-        矩阵中第k小的元素
+        输出九行，每行九个空格隔开的数字，为解出的答案。
 
-输入例子1:
-        8 3
-        1 5 9
-        10 11 13
-        12 13 15
+        输入例子1:                     输出例子1:
+        0 9 0 0 0 0 0 6 0               7 9 3 8 5 1 4 6 2
+        8 0 1 0 0 0 5 0 9               8 4 1 2 6 7 5 3 9
+        0 5 0 3 0 4 0 7 0               6 5 2 3 9 4 1 7 8
+        0 0 8 0 7 0 9 0 0               3 2 8 4 7 6 9 5 1
+        0 0 0 9 0 8 0 0 0               5 7 4 9 1 8 6 2 3
+        0 0 6 0 2 0 7 0 0               9 1 6 5 2 3 7 8 4
+        0 8 0 7 0 5 0 4 0               1 8 9 7 3 5 2 4 6
+        2 0 5 0 0 0 8 0 7               2 3 5 6 4 9 8 1 7
+        0 6 0 0 0 0 0 9 0               4 6 7 1 8 2 3 9 5
 
-输出例子1:
-        13*/
+*/
+class T2 {
+    boolean[][] rows = new boolean[9][9];
+    boolean[][] cols = new boolean[9][9];
+    boolean[][] boxes = new boolean[9][9];
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class T2 {
-
-    public static int solution(int[][] m,int k){
-        //int cur=1;
-
-
-        return 0;
-    }
-
-
-    public static void main(String[] args) {
-        int k=0,n=0;
-        Scanner scanner=new Scanner(System.in);
-        k=scanner.nextInt();
-        n=scanner.nextInt();
-        int[][] m=new int[n][n];
-//        for(int i=0;i<n;i++){
-//            for(int j=0;j<n;j++){
-//                m[i][j]=scanner.nextInt();
-//            }
-//        }
-        int[] t=new int[n*n];
-        for(int i=0;i<n*n;i++){
-            t[i]=scanner.nextInt();
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                rows[i][board[i][j] - '1'] = true;
+                cols[j][board[i][j] - '1'] = true;
+                boxes[(i / 3) * 3 + j / 3][board[i][j] - '1'] = true;
+            }
         }
-        Arrays.sort(t);
 
-        System.out.println(t[k-1]);
+        boolean b = solve(board, 0);
 
     }
 
+    public boolean solve(char[][] board, int index) {
+        if (index == 81) return true;
+
+        int r = index / 9;
+        int c = index % 9;
+        int b = (r / 3) * 3 + c / 3;
+
+        if (board[r][c] != '.') {
+            return solve(board, index + 1);
+        } else {
+            for (int i = 0; i < 9; i++) {
+                if (!rows[r][i] && !cols[c][i] && !boxes[b][i]) {
+                    board[r][c] = (char) (i + '1');
+                    rows[r][i] = true;
+                    cols[c][i] = true;
+                    boxes[b][i] = true;
+                    if (solve(board, index + 1)) return true;
+                    rows[r][i] = false;
+                    cols[c][i] = false;
+                    boxes[b][i] = false;
+                    board[r][c] = '.';
+                }
+            }
+        }
+        return false;
+    }
 }
