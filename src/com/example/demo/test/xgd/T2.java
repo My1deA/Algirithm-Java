265a2b0ea084254093fd8200fb474a10d8698456
Original <2718771714@qq.com>
2020-09-22 20:24:42 +0800

    Algorithm

---
+++ src/com/example/demo/test/xgd/T2.java
@@ -0,0 +1,11 @@
+package com.example.demo.test.xgd;
+
+import java.util.Scanner;
+
+public class T2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+
+    }
+}
